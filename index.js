28b7cdd281fbd256e8f747efdbec023a0252b05c
Justin Saborouh <rubikmanjustin@gmail.com>
2022-09-01 14:20:40 -0700

    commit tree b266525a3675ce4f07f412a5a571a7f221db4b7d

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const btn = document.getElementById("button");
+    function clickAlert(){
+        alert('I was clicked!');
+    }
+    btn.addEventListener('click', clickAlert);
+
 }
